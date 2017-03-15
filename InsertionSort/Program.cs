f2632ae21b27babebac329f790831785f1178b0a
sercan.timocin <sercan.timocin@IT-102>
2017-03-15 21:20:43 +0300

    new

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] array = Array.ConvertAll(temp, Int32.Parse);
+
+            int tempItem = array[array.Length - 1];
+
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int current = array[i];
+                int before = array[i - 1];
+
+                if (tempItem < before)
+                {
+                    array[i] = before;
+                }
+                else
+                {
+                    array[i] = tempItem;
+                    tempItem = before;
+                }
+
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Write(array[0]);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IntroToTutorialChallenges/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToTutorialChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] array = Array.ConvertAll(temp, Int32.Parse);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == v)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
