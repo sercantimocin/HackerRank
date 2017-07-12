b375d1ebaaf6dc0e799dcb5eecc5025da3eab844
sercantimocin <sercantimocin@hotmail.com>
2017-07-12 23:50:58 +0300

    treversal

--- MinimumAbsoluteDifferenceInArray/Program.cs
+++ MinimumAbsoluteDifferenceInArray/Program.cs
@@ -10,21 +10,58 @@ namespace MinimumAbsoluteDifferenceInArray
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //string[] a_temp = Console.ReadLine().Split(' ');
+            //int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            //var newa = a.OrderBy(x => x).ToArray();
+
+            //int minAbsoluteDiff = int.MaxValue;
+
+            //for (int i = 0; i < newa.Length - 1; i++)
+            //{
+            //    minAbsoluteDiff = Math.Min(minAbsoluteDiff, Math.Abs(newa[i] - newa[i + 1]));
+            //}
+
+            //Console.WriteLine(minAbsoluteDiff);
+            //Console.ReadKey();
+
+            int[][] array = new int[6][];
+
             string[] a_temp = Console.ReadLine().Split(' ');
-            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            array[0] = Array.ConvertAll(a_temp, Int32.Parse);
+            a_temp = Console.ReadLine().Split(' ');
+            array[1] = Array.ConvertAll(a_temp, Int32.Parse);
+            a_temp = Console.ReadLine().Split(' ');
+            array[2] = Array.ConvertAll(a_temp, Int32.Parse);
+            a_temp = Console.ReadLine().Split(' ');
+            array[3] = Array.ConvertAll(a_temp, Int32.Parse);
+            a_temp = Console.ReadLine().Split(' ');
+            array[4] = Array.ConvertAll(a_temp, Int32.Parse);
+            a_temp = Console.ReadLine().Split(' ');
+            array[5] = Array.ConvertAll(a_temp, Int32.Parse);
 
-            var newa = a.OrderBy(x => x).ToArray();
+            int square = 0;
+            int rectangle = 0;
+            int polygon = 0;
 
-            int minAbsoluteDiff = int.MaxValue;
-
-            for (int i = 0; i < newa.Length - 1; i++)
+            for (int i = 0; i < 6; i++)
             {
-                minAbsoluteDiff = Math.Min(minAbsoluteDiff, Math.Abs(newa[i] - newa[i + 1]));
+
+                if (array[i][0] == array[i][1] == array[i][2] == array[i][3]){
+                    square++;
+                }
+                else if ((array[i][0] == array[i][2]) && (array[i][1] == array[i][3]))
+                {
+                    rectangle++;
+                }
+                else
+                {
+                    polygon++;
+                }
             }
 
-            Console.WriteLine(minAbsoluteDiff);
-            Console.ReadKey();
+            Console.WriteLine("{0} {1}",1,2);
         }
     }
 }
---
+++ PreorderTraversal/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace PreorderTraversal
+{
+    using System;
+
+    class Program
+    {
+        class Node
+        {
+            internal int data;
+
+            internal Node left;
+
+            internal Node right;
+        }
+
+        static void Main(string[] args)
+        {
+            var root = new Node()
+                           {
+                               data = 1,
+                               left = null,
+                               right =
+                                   new Node()
+                                       {
+                                           data = 2,
+                                           left = null,
+                                           right =
+                                               new Node()
+                                                   {
+                                                       data = 5,
+                                                       left =
+                                                           new Node()
+                                                               {
+                                                                   data = 3,
+                                                                   left = null,
+                                                                   right =
+                                                                       new Node()
+                                                                           {
+                                                                               data = 4,
+                                                                               left = null,
+                                                                               right = null
+                                                                           }
+                                                               },
+                                                       right =
+                                                           new Node()
+                                                               {
+                                                                   data = 6,
+                                                                   left = null,
+                                                                   right = null
+                                                               }
+                                                   }
+                                       }
+                           };
+
+            preOrder(root);
+            Console.WriteLine();
+            postOrder(root);
+            Console.ReadKey();
+        }
+
+        static void preOrder(Node root)
+        {
+            Console.Write(root.data + " ");
+
+            if (root.left != null)
+            {
+                preOrder(root.left);
+            }
+
+            if (root.right != null)
+            {
+                preOrder(root.right);
+            }
+        }
+
+        static void postOrder(Node root)
+        {
+            if (root.left != null)
+            {
+                postOrder(root.left);
+            }
+
+            if (root.right != null)
+            {
+                postOrder(root.right);
+            }
+
+            Console.Write(root.data + " ");
+        }
+
+        static void inOrder(Node root)
+        {
+            if (root.left != null)
+            {
+                inOrder(root.left);
+            }
+
+            Console.Write(root.data + " ");
+
+            if (root.right != null)
+            {
+                inOrder(root.right);
+            }
+        }
+
+    }
+}
