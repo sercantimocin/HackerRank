34af77f2e4c62029061c236ccdbd14dadc44be94
sercantimocin <sercantimocin@hotmail.com>
2018-02-10 17:06:51 +0300

    level order traversal

--- DataStructure/LevelOrderTraversal/Program.cs
+++ DataStructure/LevelOrderTraversal/Program.cs
@@ -5,59 +5,51 @@
 
     using CommonDataStructure;
 
-    class Program
+    internal class Program
     {
 
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Node node = null;
 
             node = "1 2 5 3 6 4".Split(' ').ConvertToTree();
-            node = "5 3 10 2 4 7 12 8".Split(' ').ConvertToTree();
-            node = "1 2 3 4".Split(' ').ConvertToTree();
-            node = "5 4 7 3 9 8".Split(' ').ConvertToTree();
+            //node = "5 3 10 2 4 7 12 8".Split(' ').ConvertToTree();
+            //node = "1 2 3 4".Split(' ').ConvertToTree();
+            //node = "5 4 7 3 9 8".Split(' ').ConvertToTree();
 
             LevelOrder(node);
 
             Console.ReadKey();
         }
 
-        static void LevelOrder(Node root)
+        private static void LevelOrder(Node root)
         {
-            Queue<int> queue = new Queue<int>();
-
-            if (root != null)
+            if (root == null)
             {
-                queue.Enqueue(root.data);
+                return;
             }
 
-            LevelOrderTraversal(root, queue);
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
 
             while (queue.Count > 0)
             {
-                Console.Write(queue.Dequeue() + " ");
-            }
-        }
+                Node current = queue.Dequeue();
+                Console.Write(current.data + " ");
 
-        static void LevelOrderTraversal(Node root, Queue<int> queue)
-        {
-            if (root != null)
-            {
-                Node leftNode = root.left;
+                Node leftNode = current.left;
                 if (leftNode != null)
                 {
-                    queue.Enqueue(leftNode.data);
-                }
-                
-                Node rightNode = root.right;
-                if (rightNode != null)
-                {
-                    queue.Enqueue(rightNode.data);
+                    queue.Enqueue(leftNode);
                 }
 
-                LevelOrderTraversal(leftNode, queue);
-                LevelOrderTraversal(rightNode, queue);
+                Node rightNode = current.right;
+                if (rightNode != null)
+                {
+                    queue.Enqueue(rightNode);
+                }
             }
         }
     }
@@ -67,38 +59,27 @@
 //void levelOrder(Node root)
 //{
 
-//    Queue<Integer> queue = new ArrayDeque<Integer>();
-
-//    if (root != null)
+//    if (root == null)
 //    {
-//        queue.add(root.data);
+//        return;
 //    }
 
-//    LevelOrderTraversal(root, queue);
+//    Queue<Node> q = new LinkedList();
+//    q.add(root);
 
-//    while (queue.size() > 0)
+//    while (!q.isEmpty())
 //    {
-//        System.out.print(queue.remove() + " ");
-//    }
-//}
+//        Node current = q.remove();
+//        System.out.print(current.data + " ");
 
-//void LevelOrderTraversal(Node root, Queue<Integer> queue)
-//{
-//    if (root != null)
-//    {
-//        Node leftNode = root.left;
-//        if (leftNode != null)
+//        if (current.left != null)
 //        {
-//            queue.add(leftNode.data);
+//            q.add(current.left);
 //        }
 
-//        Node rightNode = root.right;
-//        if (rightNode != null)
+//        if (current.right != null)
 //        {
-//            queue.add(rightNode.data);
+//            q.add(current.right);
 //        }
-
-//        LevelOrderTraversal(leftNode, queue);
-//        LevelOrderTraversal(rightNode, queue);
 //    }
 //}
