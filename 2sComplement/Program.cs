57d0fed37e505d90f191c8e0cf4f07290bf42712
sercan.timocin <sercan.timocin@IT-102>
2018-02-14 18:37:55 +0300

    2compliment

---
+++ 2sComplement/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2sComplement
+{
+    class Program
+    {
+        private static readonly int MAX_VALUE_OF_32_BIT = (int)Math.Pow(2, 33);
+
+        static void Main(string[] args)
+        {
+
+            int val = -2;
+
+            int complimented = Get2Compliment(val);
+
+            List<int> list = new List<int>();
+            ConvertBinaryPresantation(complimented, list);
+
+            Console.WriteLine(list.Count(x => x == 1));
+            Console.ReadKey();
+        }
+
+        static int Get2Compliment(int value)
+        {
+            if (value > 0)
+            {
+                return MAX_VALUE_OF_32_BIT - value;
+            }
+            else
+            {
+                return MAX_VALUE_OF_32_BIT - Math.Abs(value);
+            }
+        }
+
+        static void ConvertBinaryPresantation(int value, List<int> result)
+        {
+            if (value < 2)
+            {
+                result.Add(value);
+            }
+            else
+            {
+                int remainder;
+                int quotient = Math.DivRem(value, 2, out remainder);
+                result.Add(remainder);
+
+                ConvertBinaryPresantation(quotient, result);
+            }
+        }
+    }
+}
