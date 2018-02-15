3a55d17060fb0fca9ab8eed027b92143d0f1a89c
sercan.timocin <sercan.timocin@IT-102>
2018-02-15 18:24:25 +0300

    final

--- 2sComplement/Program.cs
+++ 2sComplement/Program.cs
@@ -15,42 +15,88 @@ namespace _2sComplement
 
             int val = -2;
 
-            long complimented = Get2Compliment(val);
-
-            List<int> list = new List<int>();
-            ConvertBinaryPresantation(complimented, list);
-
-            Console.WriteLine(list.Count(x => x == 1));
+            Console.WriteLine(ConvertBinaryPresantation(val));
             Console.ReadKey();
         }
 
-        static long Get2Compliment(long value)
+        static int ConvertBinaryPresantation(int value)
         {
+            long newVal = value;
             if (value < 0)
             {
-                return (MAX_VALUE_OF_32_BIT - Math.Abs(value));
+                newVal = (long)(Math.Pow(2, 32) - Math.Abs(value));
+            }
 
+            if (newVal < 2)
+            {
+                return (int)newVal;
             }
             else
             {
-                return value;
-            }
-        }
+                int quotient = (int)(newVal / 2);
+                int remainder = (int)newVal % 2;
 
-        static void ConvertBinaryPresantation(long value, List<int> result)
-        {
-            if (value < 2)
-            {
-                result.Add((int)value);
-            }
-            else
-            {
-                long remainder;
-                long quotient = Math.DivRem(value, 2, out remainder);
-                result.Add((int)remainder);
-
-                ConvertBinaryPresantation(quotient, result);
+                return ConvertBinaryPresantation(quotient) + remainder;
             }
         }
     }
 }
+
+//import java.io.*;
+//import java.util.*;
+//import java.text.*;
+//import java.math.*;
+//import java.util.regex.*;
+
+//public class Solution
+//{
+
+//    public static void main(String[] args)
+//    {
+//        Scanner in = new Scanner(System.in);
+//        int m = in.nextInt();
+
+//        Integer[][] inputs = new Integer[m][2];
+
+//        for (int i = 0; i < m; i++)
+//        {
+//            String[] str = in.nextLine().split(" ");
+//            inputs[i][0] = Integer.parseInt(str[0]);
+//            inputs[i][1] = Integer.parseInt(str[1]);
+//        }
+
+//        for (int i = 0; i < m; i++)
+//        {
+//            int countOfOnes = 0;
+
+//            for (int j = inputs[i][0]; j <= inputs[i][1]; j++)
+//            {
+//                countOfOnes += ConvertBinaryPresantation(j);
+//            }
+
+//            System.out.println(countOfOnes);
+//        }
+
+//    }
+
+//    static int ConvertBinaryPresantation(int value)
+//    {
+//        long newVal = value;
+//        if (value < 0)
+//        {
+//            newVal = (long)(Math.pow(2, 32) - Math.abs(value));
+//        }
+
+//        if (newVal < 2)
+//        {
+//            return (int)newVal;
+//        }
+//        else
+//        {
+//            int quotient = (int)(newVal / 2);
+//            int remainder = (int)newVal % 2;
+
+//            return ConvertBinaryPresantation(quotient) + remainder;
+//        }
+//    }
+//}
