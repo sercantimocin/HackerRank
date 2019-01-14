885fd81acdb3fafb902e06d9502c40edaac4b33c
sercantimocin <sercantimocin@gmail.com>
2019-01-14 19:07:18 +0300

    linked list

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -52,6 +52,47 @@ namespace LinkedList
             }
         }
 
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+
         static void PrintDoublyLinkedList(DoublyLinkedListNode node, string sep, TextWriter textWriter)
         {
             while (node != null)
@@ -108,5 +149,45 @@ namespace LinkedList
             return head;
 
         }
+
+        static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2)
+        {
+
+            var s1 = new Stack<int>();
+            var c1 = head1.next;
+            while (c1 != null)
+            {
+                s1.Push(c1.data);
+                c1 = c1.next;
+            }
+
+            var s2 = new Stack<int>();
+            var c2 = head2.next;
+            while (c2 != null)
+            {
+                s2.Push(c2.data);
+                c2 = c2.next;
+            }
+
+            int result = s1.Count > s2.Count ? s1.Peek() : s2.Peek();
+
+            while (s1.Count > 0 && s2.Count > 0)
+            {
+                var d1 = s1.Pop();
+                var d2 = s2.Pop();
+
+                if (d1 == d2)
+                {
+                    result = d1;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+
+            return result;
+        }
+
     }
 }
