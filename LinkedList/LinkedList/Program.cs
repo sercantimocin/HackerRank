8824a413bf0344b455d421b5c110c1a1583c796b
sercantimocin <sercantimocin@gmail.com>
2019-01-15 19:18:12 +0300

    min swap count

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -110,12 +110,15 @@ namespace LinkedList
 
         static void Main(string[] args)
         {
-            var list = new DoublyLinkedList();
-            list.InsertNode(1);
-            list.InsertNode(2);
-            list.InsertNode(3);
+            //var list = new DoublyLinkedList();
+            //list.InsertNode(1);
+            //list.InsertNode(2);
+            //list.InsertNode(3);
 
-            reverse(list.head);
+            //reverse(list.head);
+
+            var a = new int[] { 4, 3, 1, 2 };
+            minimumSwaps(a);
         }
 
         static DoublyLinkedListNode reverse(DoublyLinkedListNode head)
@@ -187,6 +190,122 @@ namespace LinkedList
             }
 
             return result;
+        }
+
+
+        //bool has_cycle(Node* head)
+        //{
+
+        //    if (head == NULL || head->next == NULL)
+        //    {
+        //        return false;
+        //    }
+
+        //    Node* p1 = head;
+        //    int counter = 0;
+
+        //    while (p1 != NULL)
+        //    {
+        //        counter++;
+        //        if (counter > 100)
+        //        {
+        //            return true;
+        //        }
+
+        //        p1 = p1->next;
+        //    }
+
+        //    return false;
+        //}
+
+        //V1
+        //static int minimumSwaps(int[] arr)
+        //{
+
+        //    int swapCount = 0;
+        //    int min = Int32.MaxValue;
+        //    int minIndex = -1;
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        if (arr[i] < min)
+        //        {
+        //            min = arr[i];
+        //            minIndex = i;
+        //        }
+        //    }
+
+        //    if (minIndex > 0)
+        //    {
+        //        int t = arr[0];
+        //        arr[0] = arr[minIndex];
+        //        arr[minIndex] = t;
+        //        swapCount++;
+        //    }
+
+        //    for (int i = 1; i < arr.Length - 1; i++)
+        //    {
+        //        for (int j = i + 1; j < arr.Length; j++)
+        //        {
+        //            if (arr[j] == min + i)
+        //            {
+        //                if (j != i)
+        //                {
+        //                    int t = arr[i];
+        //                    arr[i] = arr[j];
+        //                    arr[j] = t;
+        //                    swapCount++;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return swapCount;
+        //}
+
+        // https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+        //V2
+        static int minimumSwaps(int[] arr)
+        {
+
+            int swapCount = 0;
+            int min = Int32.MaxValue;
+            int minIndex = -1;
+            var arrDic = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                    minIndex = i;
+                }
+                arrDic.Add(arr[i], i);
+            }
+
+            if (minIndex > 0)
+            {
+                int t = arr[0];
+                arr[0] = arr[minIndex];
+                arr[minIndex] = t;
+                swapCount++;
+                arrDic[arr[minIndex]] = minIndex;
+            }
+
+            for (int i = 1; i < arr.Length - 1; i++)
+            {
+                int j = arrDic[min + i];
+
+                if (j != i)
+                {
+                    int t = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = t;
+                    swapCount++;
+                    arrDic[arr[j]] = j;
+                }
+            }
+
+            return swapCount;
         }
 
     }
